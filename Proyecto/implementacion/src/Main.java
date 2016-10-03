1eddf3e6269ebe5569c9e8f42fdfa0e1a5f8e05f
jonathan <jobaji09@gmail.com>
2016-10-03 01:00:31 -0500

    hola

--- Proyecto/implementacion/src/Main.java
+++ Proyecto/implementacion/src/Main.java
@@ -7,8 +7,6 @@ public class Main{
     
     public static void main(String args[]){
 	List<Double> l = new ArrayList<>();
-	int tam;
-	
 	try(Scanner sc = new Scanner(new File("burma14.txt"))){
 	    while(sc.hasNextLine()){
 		String line =  sc.nextLine();
@@ -24,21 +22,19 @@ public class Main{
 	}catch(IOException e){
 	    
 	}
-	Double[] v =l.toArray(new Double[l.size()]);
-	for(int i=0;i<v.length;i++){
-	    System.out.println(v[i]);
-	}
-	System.out.println(v);
-	//System.out.println(l);
+	double[] v =l.toArray(new double[l.size()]);
 
-	//BinInteger bini = new BinInteger(10,1);
-	//Ruleta<Boolean,Integer> ru = new Ruleta<>(1,2);
-	//UniformMutation um = new UniformMutation(.001,1);
-	//OnePointCrossover<Boolean> opc = new OnePointCrossover<>(1,.001);
-	//NumGeneraciones ng = new NumGeneraciones(200);
-	//MaxFun mf = new MaxFun();
-	//Simple<Boolean,Integer> sim = new Simple<>(bini, null, opc, um, ru, mf, null, ng, 1000);
-	//gaframeworkgui.GAGUI.setGA(sim);
-	//gaframeworkgui.GAGUI.launch(gaframeworkgui.GAGUI.class);
+	
+	IntInteger bini = new IntInteger(v.length/2);
+	CAgenteViajero c = new CAgenteViajero(1,v.length/2);
+	MinFun min = new MinFun(v);
+	 OnePointCrossover one = new  OnePointCrossover(1,0.01);
+	 UniformMutation<Integer> uni = new UniformMutation<>(0.01,1,v.length/2);
+	 Ruleta<Integer,Integer> r = new Ruleta<>(1,100);
+	 NumGeneraciones<Integer,Integer> gen = new NumGeneraciones<>(100);
+	 
+// 	Simple<Boolean,Integer> sim = new Simple<>(bini, null, opc, um, ru, mf, null, ng, 1000);
+// 	gaframeworkgui.GAGUI.setGA(sim);
+// 	gaframeworkgui.GAGUI.launch(gaframeworkgui.GAGUI.class);
     }
 }
